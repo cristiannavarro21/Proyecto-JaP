f978cc77a4e7490bb6a1f509a895cef7137a02e3
cristiannavarro21 <elnavajo21@gmail.com>
2021-08-13 21:20:21 -0300

    table

--- js/products.js
+++ js/products.js
@@ -2,5 +2,24 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL)
+        .then(result => {
+
+            for (let i = 0; i < result.length; i++) {
+                
+                let row = "";
+                
+                row = ` 
+                    <tr>
+                     <td>`  + result[i].name + ` </td>
+                     <td>`  + result[i].description + ` </td>
+                     <td>`  + result[i].currency + ` </td>
+                     <td>`  + result[i].cost + ` </td>
+                     <td> <img height = "70" src = "https://github.com/cristiannavarro21/Proyecto-JaP/img/` + result[i].imgSrc + `.jpg" alt="">  </td>
+                    </tr>` ;
+
+                document.getElementById("data").innerHTML += row;
+            };
+        });
 
 });
