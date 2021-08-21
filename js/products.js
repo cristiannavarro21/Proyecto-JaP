5ff619d922786d6de4aaf66000cbc553e168a9b5
cristiannavarro21 <elnavajo21@gmail.com>
2021-08-21 11:36:48 -0300

    listado productos

--- js/products.js
+++ js/products.js
@@ -1,35 +1,40 @@
-var categoriesArray = [];
 
-function showCategoriesList(array){
+function listadoProductos(array) {
 
-    
+    let infoProductos = "";
     for (let i = 0; i < array.length; i++) {
-                
-        let row = "";
-        
-        row = ` 
-            <tr>
-             <td>`  + array[i].name + ` </td>
-             <td>`  + array[i].description + ` </td>
-             <td>`  + array[i].currency + ` </td>
-             <td>`  + array[i].cost + ` </td>
-             <td> <img height = "200" src = "https://cristiannavarro21.github.io/Proyecto-JaP/` + array[i].imgSrc + `" alt=""></td>
-            </tr>` ;
+        let category = array[i];
 
-        document.getElementById("data").innerHTML += row;
-    };
+
+        infoProductos += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + category.imgSrc + `" alt=" " class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ category.name + `</h4>
+                            <small class="text-muted">` + category.currency + ` ` + category.cost + `</small>
+                        </div>
+                        <p class="mb-1">` + category.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+    }
+    document.getElementById("data").innerHTML += infoProductos;
 };
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            categoriesArray = resultObj.data;
-            //Muestro las categorías ordenadas
-            showCategoriesList(categoriesArray);
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productos = resultObj.data;
+
+            listadoProductos(productos);
         }
     });
 });
