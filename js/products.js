f4d991882f40082a2064dd6e161f882bfadce2c5
cristiannavarro21 <elnavajo21@gmail.com>
2021-08-21 17:42:17 -0300

    js products

--- js/products.js
+++ js/products.js
@@ -3,21 +3,21 @@ function listadoProductos(array) {
 
     let infoProductos = "";
     for (let i = 0; i < array.length; i++) {
-        let category = array[i];
+        let autos = array[i];
 
 
         infoProductos += `
             <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
-                        <img src="` + category.imgSrc + `" alt=" " class="img-thumbnail">
+                        <img src="` + autos.imgSrc + `" alt=" " class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ category.name + `</h4>
-                            <small class="text-muted">` + category.currency + ` ` + category.cost + `</small>
+                            <h4 class="mb-1">`+ autos.name + `</h4>
+                            <small class="text-muted">` + autos.currency + ` ` + autos.cost + `</small>
                         </div>
-                        <p class="mb-1">` + category.description + `</p>
+                        <p class="mb-1">` + autos.description + `</p>
                     </div>
                 </div>
             </a>
