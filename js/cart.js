57c2b2a49181cc42029a7147555b4aac6a479111
cristiannavarro21 <elnavajo21@gmail.com>
2021-11-23 18:11:43 -0300

    entrega 7

--- js/cart.js
+++ js/cart.js
@@ -1,29 +1,134 @@
 var compra = {};
+var costoenv = 0;
+
+document.getElementById("pagos").addEventListener("click", modal);
+
+function modal() {
+    let numtarj = document.getElementById("numbercard");
+    let codigo = document.getElementById("CVV");
+    let fecha = document.getElementById("fechaven");
+    let transf = document.getElementById("numbertrans");
+
+    if (document.getElementById("formPagTarj").checked) {
+        transf.disabled = true;
+        numtarj.disabled = false;
+        codigo.disabled = false;
+        fecha.disabled = false;
+        transf.value = "";
+    }
+
+    if (document.getElementById("formPagTranf").checked) {
+        numtarj.disabled = true;
+        codigo.disabled = true;
+        fecha.disabled = true;
+        transf.disabled = false;
+        numtarj.value = "";
+        codigo.value = "";
+        fecha.value = "";
+    }
+}
+
+function validation() {
+    let flag = true;
+    let valdentro = document.getElementsByClassName("formuIn");
+    let valfuera = document.getElementsByClassName("formuOut");
+    let transf = document.getElementById("numbertrans").value;
+
+    //Ningun campo vacío dentro:
+    let cuentoDentro = 0;
+    for (let i = 0; i < valdentro.length; i++) {
+        const element = valdentro[i];
+        if (element.value == "" || element.value == "0") {
+            cuentoDentro += 1;
+        }
+        console.log(cuentoDentro);
+    }
+
+    if (cuentoDentro > 0) {
+        flag = false;
+        alert("Debe completar todo los datos del envio")
+    }
+
+
+    //Ningun campo vacío fuera:
+
+    let cuentaFuera = 0;
+    if (document.getElementById("formPagTarj").checked) {
+        for (let i = 0; i < valfuera.length; i++) {
+            const element = valfuera[i];
+            if (element.value == "") {
+                cuentaFuera += 1;
+            }
+            console.log(cuentaFuera);
+        }
+        if (cuentaFuera > 0) {
+            flag = false;
+            alert("Hay campos incompletos en su tarjeta de credito")
+        }
+    } else if (document.getElementById("formPagTranf").checked) {
+        if (transf == "") {
+            flag = false;
+            alert("Hay campos incompletos en su transferencias bancaria")
+        }
+    }
+    return flag;
+}
+let form = document.getElementById("myForm");
+form.addEventListener('submit', function (event) {
+    if (!validation()) {
+        event.preventDefault()
+        event.stopPropagation()
+    } else {
+        alert("Su compra fue realizada con exito")
+    }
+})
+
+
+function envio() {
+    let tipoenvio = document.getElementById("metodoEnv").value;
+    let subtotal = parseInt(document.getElementById("subTotalGeneral").innerHTML);
+
+    if (tipoenvio == 1) {
+        costoenv = (subtotal * 15) / 100;
+
+    } else if (tipoenvio == 2) {
+        costoenv = (subtotal * 7) / 100;
+    }
+    if (tipoenvio == 3) {
+        costoenv = (subtotal * 5) / 100;
+    }
+    document.getElementById("costoEnvio").innerHTML = costoenv;
+    document.getElementById("totalGeneral").innerHTML = subtotal + costoenv;
+}
+
 
 function subTotalGen() {
-    let total = 0;
-    let subge = document.getElementsByClassName("subtotalg");
+    let subtotalgen = 0;
+    subge = document.getElementsByClassName("subtotalg");
     for (let i = 0; i < subge.length; i++) {
-        total += parseInt(subge[i].innerHTML);
+        subtotalgen += parseInt(subge[i].innerHTML);
     }
-    document.getElementById("subTotalGeneral").innerHTML = total;
-    document.getElementById("totalGeneral").innerHTML = total;
+    document.getElementById("subTotalGeneral").innerHTML = subtotalgen;
+    document.getElementById("totalGeneral").innerHTML = subtotalgen;
+
 };
 
 
 function subTotalArt(a, i) {
+    let subTotalA = 0;
     let cantidad = parseInt(document.getElementById(`cant${i}`).value);
     let moneda = compra[i].currency;
 
     if (moneda == "USD") {
-        subtotal = a * 40 * cantidad
+        subTotalA = a * 40 * cantidad
     }
     else {
-        subtotal = a * cantidad
+        subTotalA = a * cantidad
     }
 
-    document.getElementById(`artSub${i}`).innerHTML = subtotal;
-    subTotalGen()
+    document.getElementById(`artSub${i}`).innerHTML = subTotalA;
+    subTotalGen();
+    envio();
 }
 
 
